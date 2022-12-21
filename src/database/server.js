b9f6322060a450fef0fc8cc54d494e82516415e7
Ankur Sinha <ankursinha03@gmail.com>
2022-12-21 20:22:18 +0530

    fix

--- src/database/server.js
+++ src/database/server.js
@@ -3,10 +3,13 @@ const mongoose = require("mongoose");
 const fs = require("fs");
 
 // checking connection to db
-mongoose.connect(process.env.DB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://matrix:matrixdev@cluster0.0epnlri.mongodb.net/test",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const db = mongoose.connection;
 db.on("error", (error) => console.log(error.message));
